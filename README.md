2313b1c126d9e2e7fb028ceb6ae5f3c6216e7459
Shlok Jain <85099963+Shlok-Jain@users.noreply.github.com>
2024-07-06 15:12:24 +0530

    added readme, and fixed bugs

---
+++ README.md
@@ -0,0 +1,25 @@
+# A Llama3 based Conversational RAG that can take a pdf as input and answer your questions.
+
+This is a simple RAG model that can take a pdf as input and answer your questions. It uses the Llama3 model from Ollama. It has a streamlit interface that can be used to interact with the model. First it takes the pdf file as input. It then extracts the text from the pdf file. Then it takes the query and first determines whether the query can can be answered without the document or not (In case it is greeting, valediction or simillar). If it can be answered without the document then it directly answers the query. If it can't be answered without the document then it uses the Llama3 model to answer the query.
+
+It divides the document into chunks of 1000 words and then uses a vector database to store chunks. WHen it gets the query it first performs a search on the vector database to get the most relevant chunks. Then it uses the Llama3 model to answer the query, using the retrieved chunks and previous conversation history as context (This makes  it a conversational RAG model).
+
+# ## Installation
+First install Ollama on your system from (https://ollama.com/)[https://ollama.com/]
+Then runn the Llama3 model using follwoing command:
+```bash
+ollama run llama3
+```
+
+# ## Install packages
+```bash
+pip install streamlit
+pip install pymupdf
+pip install langchain
+pip install langchain_community
+```
+
+# ## Run the code
+```bash
+streamlit run main.py
+```
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.llms import Ollama
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import DocArrayInMemorySearch
-import pdfplumber
 
 
 
@@ -13,28 +12,28 @@ import pdfplumber
 
 #function to determine whether to use the document for giving answer, or it is a general question.
 #this is because it was wierdly replying to general questions like greeting etc.
-def should_use_document(query, llm, prev):
+def should_use_document(query, st):
     prompt = f'''
     I am using an LLM to create a RAG application to answer questions based on pdfs.
     I will give you a query and you have to determine whether to use the document for answering the query or not.
     If you find that for answering that query the information from pdf will be required say only "YES" else if you think it is a general question answerable without document then say only "NO", If you are not sure say only "YES".
     I am also providing previous conversation for context.
     Rememner that this LLM is explicitly made to answer questions based on pdfs, so there will be only some cases where you would say no like greeting, aur saying bye etc. So ans "NO" wisely.
-    Previous conversation: {prev}
+    Previous conversation: {st.session_state.prev_conversation}
     Question: {query}
     '''
-    response = llm.invoke(prompt)
+    response = st.session_state.llm.invoke(prompt)
     return 0 if response != "YES" else 1
 
-def generate(query, vectorstore, llm, prev_conversation):
+def generate(query, st):
     # query = input("You: ")
-    if should_use_document(query, llm, prev_conversation):
-        context = vectorstore.similarity_search(query, k=5)
+    if should_use_document(query, st):
+        context = st.session_state.vectorstore.similarity_search(query, k=5)
         prompt = f'''
         You are given a pdf document and you have to answer the questions based on the document as well as the previous conversation.
         I have made a helper function to determine of you will need to use the document to answer this question or determine that it is a general question that can be answered without the document. And that function has determined that you will need to use the document.
         So use the document and previous conversation to answer the question. Say "I dont know" if you dont know.
-        Previous conversation: {prev_conversation}
+        Previous conversation: {st.session_state.prev_conversation}
         Document: {context}
         Question: {query}
         '''
@@ -43,36 +42,42 @@ def generate(query, vectorstore, llm, prev_conversation):
         You are given a pdf document and you have to answer the questions based on the document as well as the previous conversation.
         I have made a helper function to determine of you will need to use the document to answer this question or determine that it is a general question that can be answered without the document. And that function has determined that you will not need to use the document.
         So use just the previous conversation to answer the question. Say "I dont know" if you dont know.
-        Previous conversation: {prev_conversation}
+        Previous conversation: {st.session_state.prev_conversation}
         Question: {query}
         '''
-    response = llm.invoke(prompt)
+    response = st.session_state.llm.invoke(prompt)
     # print("Llama: " + response + '\n\n')
 
-    prev_conversation += f"Question: {query}\nAnswer: {response}\n"
+    st.session_state.prev_conversation += f"Question: {query}\nAnswer: {response}\n"
     return response
 
-def send_message(user_input, vectorstore, llm, prev_conversation):
+def send_message(st):
     user_message = st.session_state.user_input
     if user_message:
         st.session_state.messages.append({'role': 'user', 'text': user_message})
         
-        response = generate(user_message, vectorstore, llm, prev_conversation)
+        response = generate(user_message, st)
 
         st.session_state.messages.append({'role': 'bot', 'text': response})
         st.session_state.user_input = ''  # Clear the input field
 
 def main():
-
-    embeddings = OllamaEmbeddings(model='llama3')
-    llm = Ollama(model="llama3")
-    print("LLM initialized")
+    
+    # embeddings = OllamaEmbeddings(model='llama3')
+    # llm = Ollama(model="llama3")
+    # print("LLM initialized")
 
     prev_conversation = "This is the start of conversation."
+    if "embeddings" not in st.session_state:
+        st.session_state.embeddings = OllamaEmbeddings(model='llama3')
+    if "llm" not in st.session_state:
+        st.session_state.llm = Ollama(model="llama3")
     if "messages" not in st.session_state:
         st.session_state.messages = []
     if "prev_conversation" not in st.session_state:
-      st.session_state.prev_messages = prev_conversation
+      st.session_state.prev_conversation = prev_conversation
+    if "vectorstore" not in st.session_state:
+        st.session_state.vectorstore = None
     st.set_page_config(
         page_title="RAG application for answering questions based on pdfs",
         layout="wide",
@@ -80,7 +85,6 @@ def main():
     )
 
     uploaded_file = st.file_uploader("Upload a PDF", type="pdf")
-    vectorstore = None
     if uploaded_file is not None:
         text = ''
         with fitz.open(stream=uploaded_file.read(), filetype="pdf") as pdf_file:
@@ -88,7 +92,7 @@ def main():
                 text += pdf_file[page_num].get_text()
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
         texts = text_splitter.create_documents([text])
-        vectorstore = DocArrayInMemorySearch.from_documents(texts, embeddings)
+        st.session_state.vectorstore = DocArrayInMemorySearch.from_documents(texts, st.session_state.embeddings)
     
     st.markdown("""
         <div id="chat-container" style="height: 400px; overflow-y: auto; border: 1px solid #ccc; padding: 10px;">
@@ -100,8 +104,8 @@ def main():
         ''.join([f"<p><b>{msg['role'].capitalize()}:</b> {msg['text']}</p>" for msg in st.session_state.messages])
     ), unsafe_allow_html=True)
 
-    st.text_area("Your message:", key='user_input', on_change=lambda: send_message(st.session_state.user_input, vectorstore, llm, prev_conversation))
-    st.button("Send", on_click=lambda: send_message(st.session_state.user_input, vectorstore, llm, prev_conversation))
+    st.text_area("Your message:", key='user_input', on_change=lambda: send_message(st))
+    st.button("Send", on_click=lambda: send_message(st))
 
     components.html("""
         <script>
